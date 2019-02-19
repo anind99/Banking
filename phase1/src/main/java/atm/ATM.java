119ed1f0a7722c8305ffc7d48c94738e3c285f62
daniaismadi <dania.ismadi@mail.utoronto.ca>
2019-02-19 15:30:25 -0500

    Added new methods to ATM class.

--- phase1/src/main/java/atm/ATM.java
+++ phase1/src/main/java/atm/ATM.java
@@ -1,14 +1,39 @@
 public class ATM {
 
-    private Array bills;
+    public static int[] bills;
 
     public ATM() {
+        // Stores the bills in the ATM in an array with the following order:
+        // [5 dollar bills, 10, dollar bills, 20 dollar bills, 50 dollar bills].
+
+        this.bills = new int[4];
 
     }
 
-    public void login {
+    public void login(String username, String password) {
+        // Logs in the user.
 
     }
 
-    public 
+    public void processRequest() {
+
+    }
+
+    public void chooseAccount() {
+
+    }
+
+    public void alertManager() {
+        // Alerts the manager when the amount of any denomination goes below 20.
+
+    }
+
+    public shutDown() {
+
+    }
+
+    public void restart() {
+
+    }
+
 }
