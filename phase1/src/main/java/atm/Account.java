959dcb1f9e856fda2e586a7656d954c84e418cd8
hadizad5 <parastou.hadizadehmoghaddam@mail.utoronto.ca>
2019-02-19 15:30:23 -0500

    Defined class attributes and added constructor in Account class

--- phase1/src/main/java/atm/Account.java
+++ phase1/src/main/java/atm/Account.java
@@ -1,3 +1,17 @@
 public class Account(){
-    private Stri
+    public String acct_num;
+    public String[]last_transaction;
+    public String date_created;
+    public String acct_txt;
+
+    public Account(acct_num,date_created,acct_txt){
+        this.acct_num=acct_num;
+        this.date_created=date_created;
+        this.acct_txt=acct_txt;
+        this.last_transaction=[];
         }
+
+    public float view_balance(){
+
+        }
+}
