5ec30b9eeb0737fa3837998dee0adec2c8802700
colorincorrect <kuan.lai@mail.utoronto.ca>
2019-02-21 14:58:02 -0500

    added some stuff in User.java

--- phase1/src/main/java/atm/User.java
+++ phase1/src/main/java/atm/User.java
@@ -1,4 +1,19 @@
 package atm;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private String username;
+    private String pass;
+    private ArrayList<Account> accounts;
+
+
+    public double getNetTotal(){
+    }
+
+    public void updatePassword(String s){
+        this.pass = s;
+    }
+
 }
