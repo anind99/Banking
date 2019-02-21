064e2e2cfad2a1ddd508f8e7ee34d69e10d11f98
hadizad5 <parastou.hadizadehmoghaddam@mail.utoronto.ca>
2019-02-21 15:22:27 -0500

    Added balance attribute to Account class Added constructor to Asset class

--- phase1/src/main/java/atm/Account.java
+++ phase1/src/main/java/atm/Account.java
@@ -2,7 +2,7 @@ package atm;
 //import
 public class Account {
 
-
+        public double balance;
         public String acct_num;
         public Transaction last_transaction;
         public String date_created;
@@ -10,17 +10,19 @@ public class Account {
 
 
 
-        public Account(String acct_num, String date_created, String acct_txt){
+        public Account(String acct_num, double balance, String date_created, String acct_txt){
             this.acct_num = acct_num;
+            this.balance = balance;
             this.date_created = date_created;
             this.acct_txt = acct_txt;
-            //this.last_transaction =;
+            this.last_transaction = null;
         }
 
+        public double view_balance(){
+            return this.balance;
+        }
 
-        public float view_balance(){
-
-            return 0;
+        public void transfer_in(double amount, Account acct_outof){
 
         }
 }
--- phase1/src/main/java/atm/Asset.java
+++ phase1/src/main/java/atm/Asset.java
@@ -1,4 +1,8 @@
 package atm;
 
-public class Asset {
+public class Asset extends Account{
+    public Asset(String acct_num, double balance, String date_created, String acct_txt ){
+        super (acct_num, balance, date_created, acct_txt);
+
+   }
 }
