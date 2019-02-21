e809137f016d0ee6edb6888b1974ee3f9f799619
hadizad5 <parastou.hadizadehmoghaddam@mail.utoronto.ca>
2019-02-21 14:17:34 -0500

    Debugged Account class

--- phase1/src/main/java/atm/Account.java
+++ phase1/src/main/java/atm/Account.java
@@ -1,24 +1,26 @@
 package atm;
-
+//import
 public class Account {
 
 
         public String acct_num;
-        public String[] last_transaction;
+        public Transaction last_transaction;
         public String date_created;
         public String acct_txt;
 
 
 
-        public Account(acct_num, date_created, acct_txt){
+        public Account(String acct_num, String date_created, String acct_txt){
             this.acct_num = acct_num;
             this.date_created = date_created;
             this.acct_txt = acct_txt;
-            this.last_transaction = [];
+            //this.last_transaction =;
         }
 
 
         public float view_balance(){
+
+            return 0;
 
         }
 }
