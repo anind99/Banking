4b02099e19c7ff25f6a72869d1816dcb11f696be
isabelkerrebijn <isabel.kerrebijn@mail.utoronto.ca>
2019-02-21 15:30:09 -0500

    restock and bills in ATM setter

--- phase1/src/main/java/atm/ATM.java
+++ phase1/src/main/java/atm/ATM.java
@@ -2,15 +2,17 @@ package atm;
 
 public class ATM {
 
+    /** Stores the total amount of the bills in the ATM in an array with the following order:
+     [5 dollar bills, 10, dollar bills, 20 dollar bills, 50 dollar bills]. */
     public static int[] bills;
 
     public ATM() {
-        // Stores the bills in the ATM in an array with the following order:
-        // [5 dollar bills, 10, dollar bills, 20 dollar bills, 50 dollar bills].
-
-        this.bills = new int[4];
-
+        bills = new int[4];
     }
+
+    /** set the number of bills at array index "bill" */
+    public static void set_bills(int bill, int number){
+        bills[bill] = number;}
 
     public void login(String username, String password) {
         // Logs in the user.
@@ -25,8 +27,11 @@ public class ATM {
 
     }
 
+    /**Alerts the manager when the amount of any denomination goes below 20.*/
     public void alertManager() {
-        // Alerts the manager when the amount of any denomination goes below 20.
+        for(int i =0; i<=3; i++){
+            if(bills[i] < 20){BankManager.restock(i);}
+        }
 
     }
 
--- phase1/src/main/java/atm/BankManager.java
+++ phase1/src/main/java/atm/BankManager.java
@@ -2,8 +2,24 @@ package atm;
 
 public class BankManager {
 
-    public static String users;
+        public static String users_txt;
+
+        public BankManager(){
+            this.users_txt = "users.txt";
+        }
 
 
+        public static void restock(int index){
+           ATM.set_bills(index, 100);
+
+        }
+
+        public String create_user(){
+
+        }
+
+        public void undo_transaction{}
+
+        public void create_account{}
 
 }
