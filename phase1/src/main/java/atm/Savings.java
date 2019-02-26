c721619cb20862fb0b4340922b2559405b685e78
isabelkerrebijn <isabel.kerrebijn@mail.utoronto.ca>
2019-02-26 18:29:00 -0500

    updated methods to account for negative balances

--- phase1/src/main/java/atm/Savings.java
+++ phase1/src/main/java/atm/Savings.java
@@ -12,4 +12,12 @@ public class Savings extends Asset {
     //add the interest according the interestRate
     public void addInterest(){balance = (balance * interestRate);}
 
+    //Removing money from an asset account will decrease its balance
+    public boolean removeMoney(double amount){
+        if(balance - amount >= 0){
+                balance -= amount;
+                return true;
+        }else{return false;}
+    }
+
 }
