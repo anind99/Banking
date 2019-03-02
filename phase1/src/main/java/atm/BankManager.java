05d5926c6baa0cedd65eeae9100e54c893c3ac90
hadizad5 <parastou.hadizadehmoghaddam@mail.utoronto.ca>
2019-03-02 16:24:52 -0500

    create user in bank manager will create one of each type of account now

--- phase1/src/main/java/atm/BankManager.java
+++ phase1/src/main/java/atm/BankManager.java
@@ -42,9 +42,11 @@ public class BankManager {
             ArrayList accounts = new ArrayList<>();
             User newUser = new User(username, password, accounts);
             create_account(newUser, "credit card");
+            create_account(newUser, "loc");
+            create_account(newUser, "checking");
+            create_account(newUser, "saving");
+            //atm.
             return newUser;
-
-            //temp value to calm down compiler
         }
 
         public void undo_transaction(){}
@@ -54,17 +56,27 @@ public class BankManager {
                 CreditCard newCreditCard = new CreditCard(this.acct_counter);
                 user.accounts.add(newCreditCard);
                 this.acct_counter += 1;
-                try {
-                    output.write(this.acct_counter);
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-
             }
             if (actt_type.equalsignorecase("loc")){
-
+                LOC newLoc = new LOC(this.acct_counter);
+                user.accounts.add(newLoc);
+                this.acct_counter += 1;
+            }
+            if (actt_type.equalsignorecase("checking")){
+                Checking newChecking = new Checking(this.acct_counter);
+                user.accounts.add(newChecking);
+                this.acct_counter += 1;
+            }
+            if (actt_type.equalsignorecase("saving")){
+                Savings newSaving = new Savings(this.acct_counter);
+                user.accounts.add(newSaving);
+                this.acct_counter += 1;
             }
 
+            //update the account counter number in bankmanager.txt
+            try{
+                output.write(this.acct_counter);
+            }catch(IOException e){e.printStackTrace();}
         }
 
         public Date setDate(){
