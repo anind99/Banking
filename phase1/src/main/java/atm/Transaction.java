ca6f145ec4a84c8fb2fa2b20aeecf9d69e860d11
Anindya Auveek <anindya.auveek@mail.utoronto.ca>
2019-03-04 13:45:19 -0500

    transaction

--- phase1/src/main/java/atm/Transaction.java
+++ phase1/src/main/java/atm/Transaction.java
@@ -8,13 +8,34 @@ public class Transaction {
     String billname = null;
 
     public Transaction(Account Acc, Double Amount, String Type) {
+        /**
+         * Constructor for Transfer In/Out
+         * Type: refers to transaction type
+         * Account: refers to account which is being transfer in from/ transfered out to
+         *
+         */
         this.Type = Type;
         this.Amount = Amount;
         this.Account = Acc;
     }
 
     public Transaction(Double Amount, String Type) {
+        /**
+         * Withdraw/Deposit Constructor
+         */
         this.Type = Type;
         this.Amount = Amount;
     }
+
+    public Transaction(String Billname, Double Amount){
+        /**
+         * Constructor for pay bill
+         * Billname: Billname
+         */
+        this.billname = billname;
+        this.Type = "paybill";
+        this.Amount = Amount;
+
+    }
+
 }
