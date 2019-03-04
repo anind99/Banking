b08eea3ef2464d6d4d6abadb2396e06c58fcd0a9
Anindya Auveek <anindya.auveek@mail.utoronto.ca>
2019-03-04 13:25:55 -0500

    transaction update

--- phase1/src/main/java/atm/Transaction.java
+++ phase1/src/main/java/atm/Transaction.java
@@ -5,12 +5,12 @@ public class Transaction {
     String Type;
     Account Account = null;
     Double Amount;
+    String billname = null;
 
     public Transaction(Account Acc, Double Amount, String Type) {
         this.Type = Type;
         this.Amount = Amount;
         this.Account = Acc;
-
     }
 
     public Transaction(Double Amount, String Type) {
