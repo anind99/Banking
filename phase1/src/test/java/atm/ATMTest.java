0d41fbcf6961113578a4c13208ec703a7b28eae9
Anindya Auveek <anindya.auveek@mail.utoronto.ca>
2019-03-09 03:18:19 -0500

    test ATM

--- phase1/src/test/java/atm/ATMTest.java
+++ phase1/src/test/java/atm/ATMTest.java
@@ -1,4 +1,21 @@
 package atm;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class ATMTest {
+
+    @Before
+    public void runmain(){
+        ATM.Restart();
+        ATM.getListOfUsers().get(0).accounts.get(0).lastTransaction = null;
+    }
+
+    @Test
+    public void testshutdown() {
+        ATM.shutdown();
+    }
+
+
 }
+
