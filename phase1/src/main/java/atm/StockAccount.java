c62300ad49f216fc00ea1ec0e64e966276a136d4
isabelkerrebijn <isabel.kerrebijn@mail.utoronto.ca>
2019-03-24 17:18:24 -0400

    stockaccount

--- phase1/src/main/java/atm/StockAccount.java
+++ phase1/src/main/java/atm/StockAccount.java
@@ -1,4 +1,22 @@
 package atm;
 
-public class StockAccount extends Account {
+public class StockAccount extends Asset {
+    public StockAccount(int accountNum) {
+
+        super(accountNum);
+    }
+
+    public boolean removeMoney(double amount){
+        if(balance - amount >= 0){
+            balance -= amount;
+            return true;
+        }else{return false;}
+    }
+
+    public void payBill(double amount, String receiver) {
+        System.out.println("You cannot pay bills from a Stock account. Please try again.");
+    }
+
+
+
 }
