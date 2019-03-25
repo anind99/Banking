694cebf715603ad52f0a58b428c4ee6fb2e6d4c7
Anindya Auveek <anindya.auveek@mail.utoronto.ca>
2019-03-25 01:23:23 -0400

    broker

--- phase1/src/main/java/atm/Broker.java
+++ phase1/src/main/java/atm/Broker.java
@@ -1,9 +1,46 @@
 package atm;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
-public class Broker {
-    ArrayList<Stock> listOfStocks = new ArrayList<Stock>();
 
-    void Stock
+public class Broker {
+
+
+    public ArrayList<Stock> listofstocks1;
+    public ArrayList<Stock> listofstocks2;
+    public ArrayList<Stock> listofstocks3;
+    public ArrayList<Stock> listofstocks4;
+    public ArrayList<Stock> listofstocks5;
+
+
+    private void loadstocks() {
+        File file1 = new File(System.getProperty("user.dir") + "/phase1/src/main/Text Files/stocks1.txt");
+
+        try {
+            FileReader fr = new FileReader(file1);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            String name = line.split(",")[0];
+            Float price = Float.parseFloat(line.split(",")[1]);
+            int rating = Integer.parseInt(line.split(",")[2]);
+
+        } catch (Exception e){
+            System.out.println("error");
+        }
+    }
+    private void ldhelper(String line){
+    }
+
+    protected void buystock(User user, int portfolio, int amount) {
+
+    }
+
+    protected void sellstock(User user, int portfolio, int amount) {
+    }
+
+
 }
+
