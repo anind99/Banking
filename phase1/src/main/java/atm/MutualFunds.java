4b2250f6a3b6bc1aa446c0c5ab17d8d36c4002a1
daniaismadi <dania.ismadi@mail.utoronto.ca>
2019-03-26 20:49:25 -0400

    made bills non-static and added run class

---
+++ phase1/src/main/java/atm/MutualFunds.java
@@ -0,0 +1,11 @@
+package atm;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MutualFunds {
+    private int risk;
+    private String name;
+    private ArrayList<Stock> stocks;
+}
