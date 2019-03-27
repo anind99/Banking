f3b0588a61cc5fbdba68d055fc7950104103b167
Anindya Auveek <anindya.auveek@mail.utoronto.ca>
2019-03-27 01:35:42 -0400

    stockbroker

--- phase1/src/main/java/atm/StockBroker.java
+++ phase1/src/main/java/atm/StockBroker.java
@@ -5,13 +5,46 @@ public class StockBroker {
     private BankManager BM;
 
     void buyStocks(User user, String symbol, int shares) {
-
+        boolean bought = false;
+        boolean contains = false;
+        for (Stock st: user.investments.stockPortfolio){
+            if (st.symbol.equalsIgnoreCase(symbol)){
+                if ((st.currentPrice * shares) <= getStockAct(user).balance){
+                    getStockAct(user).balance -= st.currentPrice * shares;
+                    st.numShares += shares;
+                    bought = true;
+                }
+                contains = true;
+            }
+        }
+        if (!contains){
+            Stock st = fetchStock(symbol);
+            if (st != null){
+                if (st.currentPrice * shares <= getStockAct(user).balance){
+                    user.investments.stockPortfolio.add(st);
+                    st.numShares = shares;
+                    getStockAct(user).balance -= st.currentPrice * shares;
+                    bought = true;
+                }
+            } else {
+                System.out.println("There is no stock of symbol: "+symbol);
+            }
+        }
+        if (!bought)
+            System.out.println("Stocks not purchase because of insufficient funds or invalid symbol");
     }
 
-    Stock fetchStock(String Symbol){
+
+    private Stock fetchStock(String Symbol){
         // not implemented
         // returns a stock of Symbol symbol, that is fetched from API
-        return null;
+        Stock st = new Stock("name", Symbol, 0, 0);
+        try {
+            st.updatePrice();
+            return st;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     void sellStocks(User user, String stock, int shares) {
