9e222a045a71779f2c1de11a6804304d503e5a29
daniaismadi <dania.ismadi@mail.utoronto.ca>
2019-03-27 23:59:05 -0400

    refactored BankManagerInterface

--- phase1/src/main/java/atm/BankManagerInterface.java
+++ phase1/src/main/java/atm/BankManagerInterface.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
 
 public class BankManagerInterface extends Interface {
 
+    private Scanner scanner = new Scanner(System.in);
+
     public BankManagerInterface(ATM atm) {
         super(atm);
     }
 
     void displayManagerMenu(BankManager bm, ATM atm){
-        Scanner scanner = new Scanner(System.in);
         boolean loggedOut = false;
         boolean validselection = false;
         while (!validselection){
@@ -28,133 +29,32 @@ public class BankManagerInterface extends Interface {
             String option = scanner.next();
             switch (option) {
                 case "0": {
-                    boolean condition = false;
-                    String year = null, month = null, day = null;
-                    while(!condition){
-                        condition = true;
-                        System.out.println("Setting date:");
-                        System.out.println("Year:");
-                        year = scanner.next();
-                        System.out.println("Month:");
-                        month = scanner.next();
-                        System.out.println("Day:");
-                        day = scanner.next();
-                        try {
-                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                            sdf.parse(year + "-" + month + "-" + day);
-                        } catch (ParseException e){
-                            System.out.println("Parse failed. Is the date provided well formed?");
-                            System.out.println("Try again.");
-                            condition = false;
-                        }
-                    }
-                    atm.setDate(year + "-" + month + "-" + day);
-                    System.out.println("Date set, but note that time sensitive operations might not execute immediately " +
-                            "(such as addSavingsInterest, which happens after the system boots). Also note that the " +
-                            "date will still increment another day if you restart the system via the manager");
+                    settingDate();
                     break;
                 }
                 case "1": {
-                    System.out.println("Type the username for the new user");
-                    String username = scanner.next();
-                    System.out.println("Type the password for the new user");
-                    String password = scanner.next();
-                    atm.getBM().create_user(username, password);
+                    creatingUser();
                     validselection = true;
                     break;
                 }
                 case "2": {
-                    User user = null;
-                    boolean created = false;
-                    int count = 0;
-                    int count2 = 0;
-                    while (user == null) {
-                        if (count != 0){
-                            System.out.println("Type in the username of the user that would like to create an account: ");
-                        }
-                        String username = scanner.nextLine();
-                        for (User parameter : atm.getListOfUsers()) {
-                            if (parameter.getUsername().equals(username)) {
-                                user = parameter;
-                                CreateAccount(user);
-                                created = true;
-                                break;
-                            }
-                        }
-                        if (count2 !=0 && !created){System.out.println("The username is not valid, please try again.");}
-                        count += 1;
-                        count2 += 1;
-                    }
-
+                    creatingAccount();
                     validselection = true;
                     break;
                 }
                 case "3": {
-                    try {System.out.println(System.getProperty("user.dir"));
-                        File file = new File(System.getProperty("user.dir") + "/phase1/src/main/Text Files/alerts.txt");
-                        FileInputStream is = new FileInputStream(file);
-                        InputStreamReader isr = new InputStreamReader(is);
-                        BufferedReader r = new BufferedReader(isr);
-                        String line = "Alerts:";
-                        System.out.println(line);
-
-                        while ((line = r.readLine()) != null) {
-                            System.out.println(line);
-                        }
-                    } catch (IOException e) {
-                        System.err.println("Problem reading the file alerts.txt");
-                    }
+                    checkingAlerts();
                     validselection = true;
                     break;
                 }
                 case "4": {
-                    System.out.println("Select what type of bill to restock.");
-                    System.out.println("1. Five dollars, 2. Ten dollars, 3. Twenty dollars, 4. Fifty dollars");
-                    String dollarType = scanner.next();
-                    switch (dollarType) {
-                        case "1":
-                            bm.restock(1);
-                            break;
-                        case "2":
-                            bm.restock(2);
-                            break;
-                        case "3":
-                            bm.restock(3);
-                            break;
-                        case "4":
-                            bm.restock(4);
-                            break;
-                        default:
-                            System.out.println("There is no option " + dollarType + ". Pick a number from 1 to 4 or quit.");
-                            break;
-                    }
+                    restockingMachine(bm);
                     validselection = true;
                     break;
 
                 }
                 case "5": {
-                    User user = null;
-                    int count = 0;
-                    int count2 = 0;
-                    while (user == null) {
-                        if (count2 != 0){
-                            System.out.println("Type in the username of the user that would like to undo their last transaction: ");
-                        }
-                        String username = scanner.nextLine();
-                        System.out.println(username);
-                        for (User parameter : atm.getListOfUsers()) {
-                            if (parameter.getUsername().equals(username)) {
-                                user = parameter;
-                                break;
-                            }
-                        } if (count != 0){System.out.println("The username is not valid, please try again.");}
-                        count += 1;
-                        count2 += 1;
-                        //System.out.println("The username is not valid, please try again.");
-                    }
-
-                    Account account = selectAccount(user, "undo its last transaction", user.getAccounts());
-                    atm.getBM().undo_transaction(user, account);
+                    undoingTransaction();
                     validselection = true;
                     break;
                 }
@@ -164,10 +64,7 @@ public class BankManagerInterface extends Interface {
                     break;
                 }
                 case "7":{
-                    atm.testShutDown();
-                    System.out.println("System now shutting down");
-                    System.exit(0);
-
+                    shutDownSystem();
                 }
                 default: {
                     System.out.println("There is no option " + option + ". Pick a number from 1 to 7.");
@@ -175,5 +72,137 @@ public class BankManagerInterface extends Interface {
                 }
             }if (!loggedOut) displayManagerMenu(bm, atm);
         }
+    }
+
+    private void settingDate() {
+        boolean condition = false;
+        String year = null, month = null, day = null;
+        while(!condition){
+            condition = true;
+            System.out.println("Setting date:");
+            System.out.println("Year:");
+            year = scanner.next();
+            System.out.println("Month:");
+            month = scanner.next();
+            System.out.println("Day:");
+            day = scanner.next();
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                sdf.parse(year + "-" + month + "-" + day);
+            } catch (ParseException e){
+                System.out.println("Parse failed. Is the date provided well formed?");
+                System.out.println("Try again.");
+                condition = false;
+            }
+        }
+        atm.setDate(year + "-" + month + "-" + day);
+        System.out.println("Date set, but note that time sensitive operations might not execute immediately " +
+                "(such as addSavingsInterest, which happens after the system boots). Also note that the " +
+                "date will still increment another day if you restart the system via the manager");
+    }
+
+    private void creatingUser(){
+        System.out.println("Type the username for the new user");
+        String username = scanner.next();
+        System.out.println("Type the password for the new user");
+        String password = scanner.next();
+        atm.getBM().create_user(username, password);
+    }
+
+    private void creatingAccount() {
+        User user = null;
+        boolean created = false;
+        int count = 0;
+        int count2 = 0;
+        while (user == null) {
+            if (count != 0) {
+                System.out.println("Type in the username of the user that would like to create an account: ");
+            }
+            String username = scanner.nextLine();
+            for (User parameter : atm.getListOfUsers()) {
+                if (parameter.getUsername().equals(username)) {
+                    user = parameter;
+                    CreateAccount(user);
+                    created = true;
+                    break;
+                }
+            }
+            if (count2 != 0 && !created) {
+                System.out.println("The username is not valid, please try again.");
+            }
+            count += 1;
+            count2 += 1;
+        }
+    }
+
+    private void checkingAlerts(){
+        try {System.out.println(System.getProperty("user.dir"));
+            File file = new File(System.getProperty("user.dir") + "/phase1/src/main/Text Files/alerts.txt");
+            FileInputStream is = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader r = new BufferedReader(isr);
+            String line = "Alerts:";
+            System.out.println(line);
+
+            while ((line = r.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Problem reading the file alerts.txt");
+        }
+    }
+
+    private void restockingMachine(BankManager bm){
+        System.out.println("Select what type of bill to restock.");
+        System.out.println("1. Five dollars, 2. Ten dollars, 3. Twenty dollars, 4. Fifty dollars");
+        String dollarType = scanner.next();
+        switch (dollarType) {
+            case "1":
+                bm.restock(1);
+                break;
+            case "2":
+                bm.restock(2);
+                break;
+            case "3":
+                bm.restock(3);
+                break;
+            case "4":
+                bm.restock(4);
+                break;
+            default:
+                System.out.println("There is no option " + dollarType + ". Pick a number from 1 to 4 or quit.");
+                break;
+        }
+    }
+
+    private void undoingTransaction(){
+        User user = null;
+        int count = 0;
+        int count2 = 0;
+        while (user == null) {
+            if (count2 != 0){
+                System.out.println("Type in the username of the user that would like to undo their last transaction: ");
+            }
+            String username = scanner.nextLine();
+            System.out.println(username);
+            for (User parameter : atm.getListOfUsers()) {
+                if (parameter.getUsername().equals(username)) {
+                    user = parameter;
+                    break;
+                }
+            } if (count != 0){System.out.println("The username is not valid, please try again.");}
+            count += 1;
+            count2 += 1;
+            //System.out.println("The username is not valid, please try again.");
+        }
+
+        Account account = selectAccount(user, "undo its last transaction", user.getAccounts());
+        atm.getBM().undo_transaction(user, account);
+    }
+
+    private void shutDownSystem(){
+        atm.testShutDown();
+        System.out.println("System now shutting down");
+        System.exit(0);
     }
 }
