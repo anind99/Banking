fc40ad9b90b420348dab21e73dece38896564ea1
isabelkerrebijn <isabel.kerrebijn@mail.utoronto.ca>
2019-03-28 14:35:34 -0400

    Alan's updates to stock that would not push from his computer

--- phase1/src/main/java/atm/Stock.java
+++ phase1/src/main/java/atm/Stock.java
@@ -1,6 +1,14 @@
 package atm;
-import java.util.Random;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
 
 
 public class Stock {
@@ -36,9 +44,73 @@ public class Stock {
     }
 
 
+    void updatePrice(Calendar date){
+        URL url = null;
+        try {
+            JSONObject jsonobject = readJsonFromUrl("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY" +
+                    "&symbol=" + symbol + "&outputsize=full&apikey=07UY8RAZ5L3DZ0VT");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar yesterday = (Calendar) date.clone();
+            yesterday.add(Calendar.DATE, -1);
+            String todayString = sdf.format(date.getTime());
+            String yesterdayString = sdf.format(yesterday.getTime());
+            if (!jsonobject.getJSONObject("Time Series (Daily)").has(todayString)){
+                System.out.println("updatePrice in Stock.java failed: Cannot find dateString in JSONObject. " +
+                        "Is your ATM time ahead in the future of actual time?");
+                System.exit(-1);
+            }
 
-    void updatePrice(){
+            if (jsonobject.has("Error Message")){
+                System.out.println("Error from AlphaVantage. Is your stock symbol correct?");
+                System.exit(-1);
+            }
+            String todayPriceAsString = jsonobject.getJSONObject("Time Series (Daily)")
+                    .getJSONObject(todayString).getString("1. open");
+            String yesterdayPriceAsString = jsonobject.getJSONObject("Time Series (Daily)")
+                    .getJSONObject(yesterdayString).getString("1. open");
+
+            this.currentPrice = Double.valueOf(todayPriceAsString);
+            this.yesterdayPrice = Double.valueOf(yesterdayPriceAsString);
+
+
+        } catch (MalformedURLException e){
+            System.out.println(e.getMessage());
+            System.out.println("updatePrice failed, malformed URL. This should never happen!");
+
+
+            System.exit(-1);
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("updatePrice failed, IOException in url.openStream. This should never happen!");
+            System.out.println("Do you have the appropriate certificate installed on your JVM?");
+            System.out.println("Check the readme file for steps on how to install the certificate on your machine. ");
+
+            System.exit(-1);
+
+        }
 
     }
 
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+            //dfdf
+        }
+    }
 }
+
