c788b8d50f75a9ecd7162a0217926280c2022e05
colorincorrect <kuan.lai@mail.utoronto.ca>
2019-03-31 18:25:15 -0400

    Fixed Scanner sometimes not working

--- phase1/src/main/java/interfaces/GeneralInterface.java
+++ phase1/src/main/java/interfaces/GeneralInterface.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.*;
 
 public class GeneralInterface implements Serializable {
-    ATM atm;
+    protected ATM atm;
     transient Scanner scanner = new Scanner(System.in);
 
     public GeneralInterface(ATM atm) {
