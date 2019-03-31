0910029cf8e8d441f8a6ae7170d0f0a96f46df14
daniaismadi <dania.ismadi@mail.utoronto.ca>
2019-03-31 00:08:06 -0400

    added comments to broker

--- phase1/src/main/java/broker/Broker.java
+++ phase1/src/main/java/broker/Broker.java
@@ -2,10 +2,13 @@ package broker;
 
 import atm.*;
 import bankmanager.*;
-import broker.MutualFundsBroker;
-import broker.StockBroker;
+import account.*;
 
-
+/***
+ * Broker is a class that creates a new Broker for the ATM. A broker will be able to trade stocks and mutual funds
+ * and will be able to buy and sell into the mutual funds.
+ *
+ */
 public class Broker {
     private ATM atm;
     public StockBroker stockBroker;
