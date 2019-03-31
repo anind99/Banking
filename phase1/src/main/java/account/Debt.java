1cc82f4b0ebfe209a722cc04e460e526fff23a7c
daniaismadi <dania.ismadi@mail.utoronto.ca>
2019-03-31 00:34:45 -0400

    fixed implementation of max Debt

--- phase1/src/main/java/account/Debt.java
+++ phase1/src/main/java/account/Debt.java
@@ -4,7 +4,7 @@ import atm.ATM;
 
 public class Debt extends Account{
 
-    protected double maxDebt = -50000;
+    protected double maxDebt = 50000;
 
     public Debt(int accountNum, ATM atm) {
         super(accountNum, atm);
@@ -12,16 +12,16 @@ public class Debt extends Account{
 
     //Adding money to a debt account will decrease its balance
     public void addMoney(double amount){
-        if (checkMaxDebt(this.balance - amount)) {
-            System.out.println("This account has hit the maximum amount of debt! Transaction not possible!");
-        } else {
-            this.balance -= amount;
-        }
+        this.balance -= amount;
     }
 
     //Removing money from a debt account will increase its balance
     public void removeMoney(double amount){
-        this.balance += amount;
+        if (checkMaxDebt(this.balance += amount)) {
+            System.out.println("This account has hit the maximum amount of debt! Transaction not possible!");
+        } else {
+            this.balance += amount;
+        }
     }
 
     private boolean checkMaxDebt(double balance){
