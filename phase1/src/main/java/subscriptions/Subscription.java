59c483aa54b471cff37e912cbf6be79fcaa63c91
hadizad5 <parastou.hadizadehmoghaddam@mail.utoronto.ca>
2019-03-31 22:52:46 -0400

    jd

--- phase1/src/main/java/subscriptions/Subscription.java
+++ phase1/src/main/java/subscriptions/Subscription.java
@@ -2,29 +2,46 @@ package subscriptions;
 
 import java.io.*;
 
+/**
+ * Represents a subscription that users can subscribe to through the ATM.
+ */
 public class Subscription implements Serializable {
     /**
-     * Suscription class.
-     *
-     * Attributes:
-     * name: Name of Subscription (String)
-     * cost: Cost of Subscription (double)
+     * cost of the subscription.
      */
     private double cost;
+
+    /**
+     * Name of the subscription.
+     */
     private String name;
 
+    /**
+     * Subscription constructor.
+     * @param n name of the subcription
+     * @param c cost of the subscription
+     */
     public Subscription(String n, double c){
         this.name = n;
         this.cost = c;
     }
 
+    /**
+     *
+     * @return name of the subcription
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     *
+     * @return cost of the subcription
+     */
     public double getCost(){
         return this.cost;
     }
+
 
     private void writeObject(ObjectOutputStream oos) throws IOException {
         oos.defaultWriteObject();
